80b590584dc2048e05a1196c15358682bf8c4f56
Curious-Firdosh <thefirdosh@gmail.com>
2025-04-09 19:54:39 +0530

    Initial Chnage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -113,8 +113,7 @@ export default function Navbar() {
                 </motion.li>
               ))}
               <motion.a
-                href="/assets/resume.pdf"
-                download= 'resume.pdf'
+    
                 className="relative inline-block px-4 py-2 font-semibold group"
                 whileHover={{ scale: 1.1 }}
               >
